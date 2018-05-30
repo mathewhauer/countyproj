6fc1f2ea31649ad1485f0ed4baff191373303dd4
Mathew Hauer <hauer@uga.edu>
2018-05-30 14:40:01 -0400

    updates.

---
+++ SCRIPTS/001-basedataload.R
@@ -0,0 +1,48 @@
+###------DATA LOAD-----
+## @knitr evalbasedata
+
+GROUPING <- c("STATE", "COUNTY", "YEAR", "AGE", "RACE", "SEX")
+
+test_year = 2000
+launch_year = test_year
+SIZE<-18
+# NUMBER OF PROJECTION STEPS
+STEPS<-3
+FORLEN<-(STEPS*5)
+BASEANDSTEPS<-STEPS+1
+years <- 0
+years$YEAR <- seq(launch_year+5,launch_year+(STEPS*5), 5)
+years$YEAR <- seq(launch_year+1,launch_year+STEPS,1)
+
+
+K05_pop<- read.table("DATA/us.1969_2016.19ages.adjusted.txt") 
+K05_pop$V1 <- as.character(K05_pop$V1)
+K05_pop$YEAR <- as.numeric(substr(K05_pop$V1,1,4))
+K05_pop$STATEID <- substr(K05_pop$V1, 5,6)
+K05_pop$STATE <- substr(K05_pop$V1, 7,8)
+K05_pop$COUNTY <- substr(K05_pop$V1,9,11)
+K05_pop$REGISTRY <- substr(K05_pop$V1, 12,12)
+K05_pop$RACE <- substr(K05_pop$V1, 14,14)
+K05_pop$ORIGIN <- substr(K05_pop$V1, 15,15)
+K05_pop$SEX <- substr(K05_pop$V1, 16,16)
+K05_pop$AGE <- as.numeric(if_else(substr(K05_pop$V1, 17, 18) == "00","01",substr(K05_pop$V1, 17, 18)))
+K05_pop$POPULATION <- as.numeric(substr(K05_pop$V1, 19, 30))
+K05_pop <- K05_pop %>%
+  group_by(.dots = GROUPING) %>%
+  dplyr::summarise(POPULATION = sum(POPULATION))
+K05_pop$GEOID <- paste0(K05_pop$STATE, K05_pop$COUNTY)
+K05_pop$COUNTYRACE <- paste0(K05_pop$GEOID, "_", K05_pop$RACE)
+
+K05_launch <- K05_pop[which(K05_pop$YEAR == launch_year),] %>%
+  group_by(STATE, COUNTY, GEOID, YEAR) %>%
+  dplyr::summarise(POPULATION = sum(POPULATION)) %>%
+  ungroup()
+
+K05_launch2 <- K05_pop[which(K05_pop$YEAR %in% c(launch_year, launch_year+5, launch_year+10, launch_year+15)),]
+K05_launch2$COUNTYRACE <- paste0(K05_launch2$GEOID, "_", K05_launch2$RACE)
+K05_launch2$Var1 = paste0("a", K05_launch2$AGE)
+
+counties <- counties(cb = TRUE)
+counties <- spTransform(counties, CRS("+init=epsg:2163")) %>%
+  subset(!(STATEFP %in% c("60", "64","66", "68", "69", "70", "74","72", "78")))
+states <- states(cb=TRUE)
---
+++ SCRIPTS/2018-05-29_state-level-fert-rates.R
@@ -0,0 +1,62 @@
+
+
+###################
+### DATA PREP
+##################
+rm(list=ls())
+source('./SCRIPTS/001-basedataload.R')
+source('./SCRIPTS/001-fipscodes.R')
+K05_pop <- K05_pop %>%
+  group_by(.dots = GROUPING) %>%
+  dplyr::summarise(POPULATION = sum(POPULATION))
+K05_pop$GEOID <- paste0(K05_pop$STATE, K05_pop$COUNTY)
+K05_pop$COUNTYRACE <- paste0(K05_pop$GEOID, "_", K05_pop$RACE)
+
+races <- unique(K05_pop$RACE)
+
+z<- data.frame()
+for(this.state in stateid){
+ for(this.race in races){
+    K05t <- K05_pop[which(K05_pop$STATE == this.state & K05_pop$RACE == this.race),] %>%
+      group_by(YEAR,  STATE, RACE, SEX, AGE) %>%
+      dplyr::summarise(POPULATION = sum(POPULATION)) %>%
+      ungroup()
+    newbornst <- K05t %>%
+      filter(AGE == 1) %>% # AGE 1 = newborns.
+      group_by(STATE, RACE,YEAR)  %>%
+      dplyr::summarise(Newborns = sum(POPULATION))
+    childbearingt <- K05t %>%
+      filter(AGE %in% c(4,5,6,7,8,9,10), # women ages 15-49
+             SEX == "2" ) %>%
+      group_by(STATE, YEAR) %>%
+      dplyr::summarise(Women1550 = sum(POPULATION)) %>%
+      left_join(., newbornst) %>%
+      mutate(fertrat = Newborns/Women1550) %>%
+      filter(YEAR <= test_year)
+    childbearingt$SEX <- "2"
+    childbearingt[mapply(is.infinite, childbearingt)] <- NA
+    childbearingt[mapply(is.nan, childbearingt)] <- NA
+    childbearingt[is.na(childbearingt)] <-0
+    
+    predcwr = function(ccr, sex, x, DF){
+      hyndtran = function(ccr,DF){log((ccr - a) / (b - ccr))}
+      b <- max(DF[[as.character(ccr)]][which(DF$RACE== x)])*1.01
+      a <- -0.00000001
+      y <-as_data_frame(hyndtran(DF[[as.character(ccr)]][which(DF$STATE== x & DF$SEX == sex & DF$RACE == this.race)]))
+      
+      num <- seq(1,FORLEN,5)
+      pred<- tryCatch(round(predict(ucm(value~0, data = y, level = TRUE, slope = FALSE)$model, n.ahead = FORLEN)[c(num),],5)
+                      , error=function(e) array(hyndtran(DF$fertrat[which.max(DF$YEAR)]), c(STEPS)))
+      pred2 <-(b-a)*exp(pred)/(1+exp(pred))+a
+      #pred2<-ifelse(is.na(pred2), DF$fertrat[which.max(DF$YEAR)], pred2)
+      return(round(pred2,6))#
+    }
+    # n01 <- cbind(predcwr("fertrat", "2", this.race, childbearingt),0,0) 
+    z<-rbind(z,as_data_frame(predcwr("fertrat", "2", this.race, childbearingt)) %>%
+      mutate(STATE = this.state,
+             RACE= this.race,
+             SEX = 2))
+    }
+}
+
+write_csv(z, "DATA-PROCESSED/2018-05-29_state-level-fert-rates.csv")
